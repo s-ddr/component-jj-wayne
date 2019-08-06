1136f362ccfee1809f18d267b9aec52d350137ab
wayne hwang <whthwang@gmail.com>
2019-08-06 14:19:28 -0700

    [ops] updated main in package.json so that it would properly connect, and also disabled app.use(express.static

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/api/product/:id', cors(), get);
 
