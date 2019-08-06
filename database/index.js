bf68b6cc187817a99c4a9d76aabbef5f2ee11b7d
wayne hwang <whthwang@gmail.com>
2019-08-06 16:09:34 -0700

    [ops] changed localhost to remote ip address of the EC2 server

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/carousel',  { useNewUrlParser: true });
+mongoose.connect('mongodb://172.31.15.61:27017/carousel',  { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
