56f54811e8185db0fc2fe0cec1d445f4e7cd3b95
wayne hwang <whthwang@gmail.com>
2019-08-06 21:23:45 -0700

    [ops] changing port number

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://172.31.1.44:27017/carousel',  { useNewUrlParser: true });
+mongoose.connect('mongodb://172.31.9.131:27017/carousel',  { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
