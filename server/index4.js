06e88770af3a88243c991809cf333676eb17f9ce
wayne hwang <whthwang@gmail.com>
2019-08-09 00:20:31 -0700

    [ops] added 4th server for load balancing

---
+++ server/index4.js
@@ -0,0 +1,31 @@
+const newRelic = require('newrelic');
+const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+const cors = require('cors')
+const { get } = require('./controllers.js');
+
+const app = express();
+const port = 3007;
+
+app.get('*.js.gz', (req, res, next) => {
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Request-Headers", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/product/:id', cors(), get);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
